ec67268ae2f38b7fd78f3e767116db468d0a7ae7
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-12-13 09:55:47 +0100

    search bar
    per non sentire la fine del mondo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FirestoreModule } from '@angular/fire/firestore';
 import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 import { createUserWithEmailAndPassword, User } from 'firebase/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,12 @@ import { createUserWithEmailAndPassword, User } from 'firebase/auth';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private auth: Auth, private router: Router) {}
 
-  constructor(private auth: Auth) {
-   
-}
+  searchUser(userName: HTMLInputElement) {
+    let valueUserName = userName.value;
+    this.router.navigate(['/profile/', valueUserName]);
+  }
 
 /* ngOnInit(auth: Auth) {
   if(auth.currentUser) {
