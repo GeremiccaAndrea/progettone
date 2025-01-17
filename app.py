b927de28ae21fed1b6b095493768b975079bd30c
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2025-01-17 12:44:45 +0000

    restituisce file Geojson

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
-import pandas as pd 
-import geopandas as gpd
-import requests
-from shapely.geometry import Point,MultiPolygon, Polygon
-import matplotlib.pyplot as plt
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
-import json
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 from bson.json_util import dumps
+import json
+
+# Configurazione del database MongoDB
 uri = "mongodb+srv://classeIntera:loto@safezone.lrtrk.mongodb.net/?retryWrites=true&w=majority&appName=safezone"
-# Create a new client and connect to the server
 client = MongoClient(uri)
-db=client["mappaUtenti"]
+db = client["mappaUtenti"]
+
+# Configurazione Flask
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
+
 @app.route('/GetCity/<cityName>')
 def getCity(cityName):
-    cityName=cityName.capitalize()
+    cityName = cityName.capitalize()
     if cityName in map(str.capitalize, db.list_collection_names()):
-        collection=db[cityName]
+        collection = db[cityName]
         documenti = list(collection.find())
-        return jsonify(json.loads(dumps(documenti)))
+        
+        # Creazione del GeoJSON
+        geojson = {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": doc.get("geometry", {}),
+                    "properties": {key: value for key, value in doc.items() if key not in ["_id", "geometry"]}
+                }
+                for doc in documenti
+            ]
+        }
+        return jsonify(geojson)
     else:
-        return "error, city not found!! :("
-
+        return jsonify({"error": "City not found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
