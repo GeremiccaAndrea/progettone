276efb977dc579a1848c83ffe37dbb1a8b94abb3
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2025-01-31 09:54:34 +0000

    aggiunta query per prendere dati dei crimini

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from pymongo.mongo_client import MongoClient
 from bson.json_util import dumps
 import json
+from datetime import datetime, timedelta
 
 # Configurazione del database MongoDB
 uri = "mongodb+srv://classeIntera:loto@safezone.lrtrk.mongodb.net/?retryWrites=true&w=majority&appName=safezone"
@@ -18,7 +19,17 @@ def getCity(cityName):
     cityName = cityName.capitalize()
     collection = db["Geometry"]
     ListaCittà= collection.distinct("città")
-    print(ListaCittà)
+    collectionCrimini=db["crimini"]
+    #trovo la data di oggi
+    today = datetime.utcnow()
+    ten_years_ago = today - timedelta(days=10*365)  # Circa 10 anni
+    print(ten_years_ago)
+    query = {
+        "citta": cityName,
+        "data": {"$gte": ten_years_ago.isoformat()}  # Confronto con la stringa ISO 8601
+    }
+    recent_crimes = list(collection.find(query))
+    return jsonify(recent_crimes)
     if cityName in ListaCittà:
         documenti = list(collection.find({"città": cityName}))
         
